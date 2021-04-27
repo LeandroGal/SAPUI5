0cf8b8a93a1ea516b1beffa18f686ba3c18b36d1
LeandroGal <lean.galante@gmail.com>
2021-04-27 17:37:57 +0000

    07 Interfaz de usuario - Objetos

--- Invoices/SAPUI5/webapp/Component.js
+++ Invoices/SAPUI5/webapp/Component.js
@@ -1,14 +1,15 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "logaligroup/SAPUI5/model/Models",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 ],
     /**
      * @param {typeof sap.ui.core.UIComponent} UIComponent
      * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
      */
 
-    function (UIComponent, Models, ResourceModel) { // UIComponent para extender y devolver la instancia del componente de la App 
+    function (UIComponent, Models, ResourceModel, HelloDialog) { // UIComponent para extender y devolver la instancia del componente de la App 
 
         return UIComponent.extend("logaligroup.SAPUI5.Component", {
 
@@ -32,6 +33,17 @@ sap.ui.define([
                 // set i18n Model on the view
                 var i18nModel = new ResourceModel({ bundleName: "logaligroup.SAPUI5.i18n.i18n" });
                 this.setModel(i18nModel, "i18n");
+
+                this._helloDialog = new HelloDialog(this.getRootControl());
+            },
+
+            exit: function () {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+
+            openHelloDialog: function () {
+                this._helloDialog.open();
             }
         });
     });
--- Invoices/SAPUI5/webapp/controller/App.controller.js
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -1,29 +1,17 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller
-     * @param {typeof sap.m.MessageToast} MessageToast
-     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
      */
-    function (Controller, MessageToast, Models, ResourceModel) {
+    function (Controller) {
         "use strict"; // Nos advierte de posibles "errores silenciosos"
 
         return Controller.extend("logaligroup.SAPUI5.controller.App", {
 
             onInit: function () { // Cuando se inicia el objeto
 
-            },
-
-            onShowHello: function () {
-                // Read text from i18n
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                // Read property from data model
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                MessageToast.show(sMsg);
             }
         });
     })
